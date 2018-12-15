864031e03ea9716ed824d8051c614811b52cb79a
M. van Leeuwen <lemval@gmail.com>
2018-12-15 14:32:16 +0100

    Strip debugging info from build.

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@
 go clean
 go build
 
-env GOOS=linux GOARCH=arm   go build -o growatt_pi
-env GOOS=linux GOARCH=arm64 go build -o growatt_odroid
+env GOOS=linux GOARCH=arm   go build -o growatt_pi -ldflags '-s'
+env GOOS=linux GOARCH=arm64 go build -o growatt_odroid -ldflags '-s'
