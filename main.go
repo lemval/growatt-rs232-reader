6f2ade83a32cdca03ea4f650f670cfd7d0e434a1
M. van Leeuwen <lemval@gmail.com>
2018-12-24 10:02:13 +0100

    Up the release

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func init() {
 }
 
 func main() {
-	diag.Info("Starting Growatt Inverter Reader v1.002")
+	diag.Info("Starting Growatt Inverter Reader v1.003")
 
 	//	Read the command line arguments
 	flag.Parse()
