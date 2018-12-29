183a06ef2c729532a35e5b9ab600a1f95d0a7dbc
M. van Leeuwen <lemval@gmail.com>
2018-12-29 15:29:58 +0100

    For older Pi

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ go build
 
 mkdir release >/dev/null 2>&1
 
-env GOOS=linux GOARCH=arm   go build -o release/growatt_pi -ldflags '-s'
-env GOOS=linux GOARCH=arm64 go build -o release/growatt_odroid -ldflags '-s'
+env GOOS=linux GOARCH=arm GOARM=5 go build -o release/growatt_pi2 -ldflags '-s'
+env GOOS=linux GOARCH=arm         go build -o release/growatt_pi -ldflags '-s'
+env GOOS=linux GOARCH=arm64       go build -o release/growatt_odroid -ldflags '-s'
